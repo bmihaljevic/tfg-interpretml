d6e85bf0ce0004b6547def34a16345d450694bcf
javipzv <90279135+javipzv@users.noreply.github.com>
2024-12-10 12:19:34 +0100

    2024-12-09 and 2024-12-10: Read about NAMs and check visualizations of categorical features in InterpretML

--- interpret/python/interpret-core/interpret/glassbox/_ebm/_ebm.py
+++ interpret/python/interpret-core/interpret/glassbox/_ebm/_ebm.py
@@ -1566,12 +1566,12 @@ class EBMModel(BaseEstimator):
         density_list = []
         keep_idxs = []
         for term_idx, feature_idxs in enumerate(self.term_features_):
-            # model_graph = mod_term_scores[term_idx]
+            model_graph = mod_term_scores[term_idx]
 
             # MODIFICATION: Constant model graph with the first value of the term scores
-            length = len(mod_term_scores[term_idx])
-            model_graph = [mod_term_scores[term_idx][0]] * length
-            model_graph = np.array(model_graph)
+            # length = len(mod_term_scores[term_idx])
+            # model_graph = [mod_term_scores[term_idx][0]] * length
+            # model_graph = np.array(model_graph)
 
             # NOTE: This uses stddev. for bounds, consider issue warnings.
             errors = mod_standard_deviations[term_idx]
@@ -1881,9 +1881,9 @@ class EBMModel(BaseEstimator):
                 data_dict = {
                     "type": "univariate",
                     "names": list(self.term_names_),
-                    # "scores": list(sample_scores),
+                    "scores": list(sample_scores),
                     # constant scores for all terms
-                    "scores": np.array([sample_scores[0]] * len(sample_scores)),
+                    # "scores": np.array([sample_scores[0]] * len(sample_scores)),
                     "values": values,
                     "extra": {
                         "names": ["Intercept"],
--- log.md
+++ log.md
@@ -1,24 +1,23 @@
 
 
-## 2024-12-04
+## 2024-12-09 and 2024-12-10
 
 ### Summary of the Day
-- Total Hours Worked: 1.5
-- Main Objective: Understanding of LDA and QDA, and its internal parameters.
+- Total Hours Worked: 2
+- Main Objective: Read about NAMs and check how categorical features are treated by EBMs explanations. 
 
 ### Tasks Completed
-1. Read documentation about LDA and QDA
-2. Created a notebook using LDA and QDA and examining its attributes.
+1. Read this introductory paper about NAMs: https://arxiv.org/abs/2004.13912
+2. Created a notebook solving a binary classification problem to see how categorical features were treated by Interpret Explanations.
 
 ### In-Progress
 - Naive Bayes Implementation and Visualization for Binary Classification.
-- Naive Bayes Global Explanation visualization.
-- Comprehension of Discriminant Analysis.
-- Implementation of Discriminant Analysis.
+- Naive Bayes Global Explanation Visualization.
+- Reading and comprehension about NAMs.
 
 ### Next Steps for Tomorrow
-- Look further into Discriminant Analysis.
-- Think about feature contribution in Discriminant Analysis.
+- Look further into NAMs.
+- Check some interesting visualizations in Yellowbrick that can be useful. 
 
 ### Issues/Blockers
 -
